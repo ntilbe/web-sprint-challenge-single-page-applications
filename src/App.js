69ff0b1d1f2b259200c32418d14ff3e3d14460cf
Nicole Tilbe <nicolecali2406@outlook.com>
2020-07-24 13:50:39 -0400

    progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,102 @@
 import React, { useState } from "react";
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import Form from './Form'
 import Home from './Home'
+import * as yup from 'yup'
+import formSchema from './validation/FormSchema'
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  pepperoni: false,
+  olives: false,
+  pineapple: false,
+  bacon: false,
+  instructions: ''
+}
+
+const initialFormErrors = {
+  name: '',
+  size: ''
+}
+
+const initialOrders = []
 
 const App = () => {
+
+  //State//
+  const [orders, setOrders] = useState(initialOrders)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+
+  const addOrder = newOrder => {
+    setOrders([newOrder])
+    setFormValues(initialFormValues)
+  }
+
+  const inputChange = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        })
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        })
+      })
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const checkboxChange = (name, isChecked) => {
+    setFormValues({
+      ...formValues,
+      hobbies: {
+        ...formValues.hobbies,
+        [name]: isChecked,
+      }
+    })
+  }
+
+  const submit = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      pepperoni: formValues.pepperoni,
+      olives: formValues.olives,
+      pineapple: formValues.pineapple,
+      bacon: formValues.bacon,
+      instructions: formValues.instructions
+    }
+
+    addOrder(newOrder)
+  }
+
   return (
     <div>
+      <header>
+        <Link to='/'>
+          <h1>Lambda Eats</h1>
+        </Link>
+      </header>
       <Switch>
         <Route path='/pizza'>
-          <Form />
+          <Form 
+          values={formValues}
+          submit={submit}
+          inputChange={inputChange}
+          checkboxChange={checkboxChange}
+          formErrors={formErrors}
+          />
         </Route>
         <Route path='/'>
           <Home />
--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,11 @@ import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <div>
-            <h1>Lambda Eats</h1>
             <div className='hero-background'>
             <Link to='/pizza'>
             <button className='pizza-btn'>Pizza?</button>
             </Link>
             </div>
-            
-            
         </div>
         
     );
